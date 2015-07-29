959d81392c352e84032dad39c4e9739494bb3440
Taylor Brown <browtayl@onid.oregonstate.edu>
2015-07-29 11:53:28 -0700

    define sad class

---
+++ src/main/groovy/edu/oregonstate/mist/sadapi/core/Sad.groovy
@@ -0,0 +1,13 @@
+package edu.oregonstate.mist.sadapi.core
+
+class Sad {
+    Long pidm
+    String termCodeEntry
+    Long applNo
+    Long seqNo
+    Date apdcDate
+    String apdcCode
+    String maintInd
+    String user
+    String dataOrigin
+}
